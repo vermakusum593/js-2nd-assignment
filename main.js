80ed065c17e0fd4d75b0e0767890d0867c0e2417
vermakusum593 <vermakusum593@gmail.com>
2024-10-21 23:27:17 +0200

    updated main.js

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
-let priceFromUser = prompt("Enter price tag", "0") 
+let priceFromUser1st = prompt("Enter price tag", "0");
+
+let priceFromUser2nd = priceFromUser1st.slice(1);
 
 let priceInNumber = parseFloat(priceFromUser);
 
