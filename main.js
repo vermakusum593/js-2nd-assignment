c8f5c01caf96da93990189cfce3a106165cb9a02
vermakusum593 <vermakusum593@gmail.com>
2024-10-22 16:18:53 +0200

    updated main.js

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ let priceFromUser1st = prompt("Enter price tag", "0");
 
 let priceFromUser2nd = priceFromUser1st.slice(1);
 
-let priceInNumber = parseFloat(priceFromUser);
+let priceInNumber = parseFloat(priceFromUser2nd);
 
 let discount = (priceInNumber * 10) / 100;
 
